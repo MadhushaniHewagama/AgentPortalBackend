5fe0c737a023e5e46e52067b4d03e386d67d6ee9
MadhushaniHewagama <madhushani.hewagama@chakray.com>
2023-06-30 09:29:28 +0530

    initial project creation

---
+++ src/test/java/com/example/agentportalbackend/AgentPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agentportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentPortalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
